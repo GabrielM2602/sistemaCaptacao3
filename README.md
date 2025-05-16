42403d09578c349bc018458eef54e789a50a6095
Gabriel <gabriemdo26@gmail.com>
2025-05-16 12:31:30 -0300

    Tentativa

---
+++ README.md
@@ -0,0 +1,1 @@
+# sistemaCaptacao2
---
+++ src/Aplicacao/Programa.java
@@ -0,0 +1,17 @@
+package Aplicacao;
+
+import Entidades.ServidorPublico;
+
+public class Programa {
+    public static void main(String[] args) {
+        ServidorPublico isabela = new ServidorPublico();
+        isabela.setNome("Isabela");
+        isabela.setCargo("Auditor");
+        isabela.setOrgao("ANVISA");
+        isabela.setLotacao("Brasilia");
+        isabela.setEmail("isabela@gmail.");
+
+        System.out.println("Servidor: "+ isabela.getNome());
+
+    }
+}
